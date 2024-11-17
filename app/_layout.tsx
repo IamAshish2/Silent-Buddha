f3ebc3ffb268a16a8a3200e2749307e338758a5a
IamAshish2 <karkiaashish9899@gmail.com>
2024-11-17 22:14:44 +0545

    add stack screens to switch tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,12 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import "../global.css"
 
 export default function RootLayout() {
     return (
-        <Slot />
+        // specify Stack Screens for switching between tabs
+        <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
     );
 }

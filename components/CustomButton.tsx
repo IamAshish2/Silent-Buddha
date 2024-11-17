6fc728de7e21306a3f7fce74d1870ed89e151de8
IamAshish2 <karkiaashish9899@gmail.com>
2024-11-17 21:10:13 +0545

    reusable custom btn component

---
+++ components/CustomButton.tsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+
+interface CustomButtonProps {
+    onPress: () => void,
+    title: string,
+    textStyles?: string,
+    contatinerStyles?: string
+}
+
+const CustomButton = ({onPress,title,textStyles,contatinerStyles}: CustomButtonProps) => {
+
+    return (
+        <TouchableOpacity 
+            activeOpacity={0.7}
+            className={`bg-white rounded-xl min-h-[62px] justify-center items-center ${contatinerStyles}`}
+            onPress={onPress}>
+                
+            <Text> {title} </Text>
+        </TouchableOpacity>
+    )
+}
+
+export default CustomButton

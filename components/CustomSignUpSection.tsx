d7ff7b3a053e5c0115c7ff5dc45e43316fae80ed
IamAshish2 <karkiaashish9899@gmail.com>
2024-12-18 21:46:33 +0545

    clear code cache. ready for google and facebook authentication

--- components/CustomSignUpSection.tsx
+++ components/CustomSignUpSection.tsx
@@ -1,16 +1,64 @@
 import { View, Text, Pressable, Image } from 'react-native'
 import { facebook, google } from '@/constants/otherImages'
 import { useRouter } from 'expo-router'
+// import auth from "@react-native-firebase/auth";
+// import {
+//     GoogleSignin,
+//     GoogleSigninButton,
+//     statusCodes,
+// } from "@react-native-google-signin/google-signin";
+// import Constants from "expo-constants";
 
 
 type CustomSignUpSectionProps = {
-    headingText: string,
-    BottomText: string
+    headingText?: string,
+    BottomText?: string
+    // promptAsync: () => void
 }
 
 {/* Create you account / Continue with fb/google section */ }
 const CustomSignUpSection = ({ headingText, BottomText }: CustomSignUpSectionProps) => {
+
     const router = useRouter();
+
+    // This is where we need to configure the webClientId
+    // Android authentication will not work without this (Google)
+    /*
+    GoogleSignin.configure({
+        webClientId:'177859685656-pp5aeak8e6u349cf64hce22ahg98l86h.apps.googleusercontent.com',
+    });
+
+    // Async function to execute the sign-in functionality
+    async function signIn() {
+        try {
+            // Checks if the device supports Play services
+            await GoogleSignin.hasPlayServices();
+
+            // Extracts the idToken from the authentication request
+            const { idToken } = await GoogleSignin.signIn();
+
+            // Create Google credentials with the idToken
+            const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+            // Login with the credentials
+            return auth().signInWithCredential(googleCredential);
+
+            // Handle error cases
+        } catch (error: any) {
+            console.log(error);
+        }
+    }
+
+
+    async function googleLogin(){
+        try{
+            const  userCred = await signIn();
+            console.log(userCred);
+        }catch(e){
+            console.log(e)
+        }
+    }
+        */
 
     return (
         <View className='mt-5 flex-col items-center justify-between h-48'>
@@ -33,7 +81,7 @@ const CustomSignUpSection = ({ headingText, BottomText }: CustomSignUpSectionPro
                 </Pressable>
 
                 <Pressable
-                    // onPress={onGoogleButtonPress}
+                    // onPress={() => googleLogin()}
                     className='h-16 w-[24rem] justify-center rounded-full border border-zinc-400'>
                     <View className="flex flex-row items-center justify-center">
                         <Image
--- lib/Firebase/firebaseConfig.ts
+++ lib/Firebase/firebaseConfig.ts
@@ -1,10 +1,7 @@
 import { initializeApp } from "firebase/app";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
-// Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -15,15 +12,9 @@ const firebaseConfig = {
   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Use `initializeAuth` with AsyncStorage for persistence
-const auth = initializeAuth(app, 
-  {persistence: getReactNativePersistence(ReactNativeAsyncStorage),}
-);
-
-// Initialize Firestore
+const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db, app };
